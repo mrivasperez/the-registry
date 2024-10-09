dbb313bee7dce6547a3e07672b3d5bc7a6843f61
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-10-09 15:36:45 -0700

    ui: style search results items

--- src/components/PackageListItem.tsx
+++ src/components/PackageListItem.tsx
@@ -10,27 +10,28 @@ const PackageListItem = ({ data }: PackageListItemProps) => {
   const renderedKeywords = (data.keywords || []).map((keyword) => (
     <div
       key={keyword}
-      className="border py-0.5 px-1 text-xs bg-slate-200 rounded"
+      className="block py-0.5 px-1 text-xs bg-blue-100 rounded h-full w-full whitespace-nowrap"
     >
       {keyword}
     </div>
   ));
 
   return (
-    <div className="border p-4 rounded flex justify-between items-center">
-      <div className="flex flex-col gap-2">
+    <div className="border p-4 rounded flex justify-between items-center  w-full hover:shadow">
+      <div className="w-2/3">
         <Link to={`/packages/${data.name}`}>{data.name}</Link>
-        <p className="text-sm text-gray-500">{data.description}</p>
-        <div className="flex gap-1">{renderedKeywords}</div>
+        <p className="text-sm text-gray-500 py-2">{data.description}</p>
+        <div className="flex gap-1 overflow-auto">
+          <i className="bi bi-key"></i>
+          {renderedKeywords}
+        </div>
       </div>
-      <div className="mr-6">
-        <Link
-          to={`/packages/${data.name}`}
-          className="py-2 px-3 rounded bg-black text-white text-lg"
-        >
-          View
-        </Link>
-      </div>
+      <Link
+        to={`/packages/${data.name}`}
+        className="py-2 px-3 rounded bg-blue-800 text-white flex text-sm"
+      >
+        View <i className="bi bi-arrow-right ms-1"></i>
+      </Link>
     </div>
   );
 };
